ad3eebc059ded26bcd314e87f9feb213b2e9277f
Amin Rois <aminrois6@gmail.com>
2022-08-01 09:47:36 +0700

    member & share

--- src/app/layout/project/member/member.component.ts
+++ src/app/layout/project/member/member.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { HttpClient, } from '@angular/common/http';
 import { Urlservice } from '../../../shared/services/url.service';
@@ -7,6 +7,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 import { Subscription } from 'rxjs';
 import { FileSaver }   from 'file-saver';
 import { send } from 'q';
+import swal from 'sweetalert2';
 
 @Component({
     selector: 'app-member',
@@ -19,6 +20,11 @@ export class MemberComponent implements OnInit {
     namaproject:any;
     datauser:any;
   datamember: any;
+  datamember2:any;
+  datacari:any;
+  datanya:any;
+  datarole:any;
+  idrole:any;
     constructor(
         private http:HttpClient,
         private url:Urlservice,
@@ -32,6 +38,8 @@ export class MemberComponent implements OnInit {
         console.log(this.namaproject);
         this.panggiluser();
         this.panggilstatusmember();
+        this.panggilrole();
+        this.idrole=0;
 
     }
     open(carimem) {
@@ -52,6 +60,30 @@ export class MemberComponent implements OnInit {
             return  `with: ${reason}`;
           }
         }
+        buka(carirole, data) {
+          this.idrole=0;
+          this.datamember2=data;
+          this.modalService.open(carirole, {ariaLabelledBy: 'app-charts'}).result.then((result) => {
+            // this.closeResult = `Closed with: ${result}`;
+            this.tambahmember();
+            // console.log(this.datamember2.nama_user);
+            
+          }, (reason) => {
+            // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          });
+        } 
+        gantirole(editrole, member) {
+          this.datamember2=member;
+          this.idrole=member.role_project.id_role_project;
+          this.modalService.open(editrole, {ariaLabelledBy: 'app-charts'}).result.then((result) => {
+            // this.closeResult = `Closed with: ${result}`;
+            // this.tambah();
+            console.log(this.datamember2);
+            
+          }, (reason) => {
+            // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          });
+        } 
     panggiluser(){
         this.http.get(this.url.apiurl+'/users').subscribe(data => {
             this.datauser=data['data'];
@@ -61,11 +93,109 @@ export class MemberComponent implements OnInit {
       })
     }
     panggilstatusmember(){
-      this.http.get(this.url.apiurl+'/member').subscribe(data=>{
+      this.http.get(this.url.apiurl+'/member/tampil?id_project='+this.dataproject.id_project).subscribe(data=>{
           this.datamember=data['data'];
           console.log(this.datamember);
       },err=>{
         console.log(err);
+      })
+    }
+    panggilrole(){
+      this.http.get(this.url.apiurl+'/role').subscribe(data=>{
+        this.datarole=data['data'];
+        console.log(this.datarole);
+    },err=>{
+      console.log(err);
+    })
+    }
+    carimember(){
+      let cari=this.datacari;
+      console.log(this.datacari)
+      this.datanya = this.datauser.filter(function(itm){
+        // console.log(itm.nama_user);
+        return itm.nama_user.toLowerCase().indexOf(cari.toLowerCase()) !== -1;
+      });
+      if(this.datacari==[]||this.datacari==undefined||this.datacari==""){
+        this.datanya=[];
+      }
+    }
+    tambahmember(){
+      if (this.idrole==0){
+        // alert('Metode Project Harus di Pilih');
+        swal.fire(
+          'Gagal!',
+          'Status Member Harus di Pilih',
+          'error'
+        )
+      }else{
+        swal.fire({
+          title: 'Apakan Anda Yakin?',
+          text: "Ingin Menambah "+this.datamember2.nama_user+" Sebagai Member Baru Project "+this.dataproject.nama_project+" ini !!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Ya, Tambah Member!',
+          cancelButtonText: 'Batal!'
+        }).then((result) => {
+          if (result.value) {
+                  let formData = new FormData();
+                  formData.append('id_project', this.dataproject.id_project);
+                  formData.append('id_user', this.datamember2.id_user);
+                  formData.append('id_role_project', this.idrole);
+                  this.http.post(this.url.apiurl+'/member', formData).subscribe(data => {
+                  console.log(data);
+                  this.panggilstatusmember();
+                  swal.fire(
+                    'Berhasi!',
+                    'Berhasil Tambah Member Baru.',
+                    'success'
+                  )
+            }, err => {
+              console.log(err);
+              alert(err)
+              swal.fire(
+                'Gagal Tambah!',
+                'Gagal Tambah Member Baru.',
+                'error'
+              )
+            })
+          }
+        })   
+      } 
+    }
+
+    hapusmember(id){
+      swal.fire({
+        title: 'Apakan Anda Yakin?',
+        text: "Anda Tidak Bisa Mengembalikan Member Ini!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ya, Hapus Member!',
+        cancelButtonText: 'Batal!'
+      }).then((result) => {
+        if (result.value) {
+          this.http.delete( this.url.apiurl+'/member'+ '/' +id ).subscribe(data => {
+            console.log(data);
+                // alert('Sukses Hapus')
+                this.panggilstatusmember();
+                swal.fire(
+                  'Terhapus!',
+                  'Member Anda Sudah Terhapus.',
+                  'success'
+                )
+          }, err => {
+            console.log(err);
+            alert(err)
+            swal.fire(
+              'Gagal Menghapus!',
+              'Member Anda Tidak Terhapus.',
+              'error'
+            )
+          })
+        }
       })
     }
 }
--- src/app/layout/share/share.component.ts
+++ src/app/layout/share/share.component.ts
@@ -20,7 +20,94 @@ import { element } from 'protractor';
     animations: [routerTransition()]
 })
 export class ShareComponent implements OnInit {
-    constructor() {}
+    closeResult: string;
+    email:any;
+    nama:any;
+    password:any;
+    datanya:Array<any>=[];
+    datasdlc:any;
+    datauser:any;
+    iduser:any;
+    idsdlc:any;
+    datanya2:Array<any>=[];
+    datanya1:any;
+    link:Array<any>=[];
+    total_pages:Array<any>=[];
+    balik:any;
+    constructor(
+        private modalService: NgbModal, 
+      private http: HttpClient, 
+      private url: Urlservice,
+      private spinner: NgxSpinnerService,
+      private alerts: AlertsService,
+      private router : Router,
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.datauser=JSON.parse(localStorage.getItem('isLoggedin'));
+        this.iduser=this.datauser[0]['id_user'];
+        this.project();    
+        // console.log(this.iduser);
+        this.idsdlc=0;
+        localStorage.removeItem('dataproject');
+    }
+
+    project(){
+        this.datanya=[];
+          this.link=[];
+          this.total_pages=[];
+      let formData = new FormData();
+          formData.append('id_user', this.iduser);
+          this.http.get(this.url.apiurl+'/member/tampiluser?id_user='+ this.iduser).subscribe(data => {
+          let datanya2 =data['data'];
+          this.datanya=datanya2;
+          this.link=data['meta'].pagination;
+          const total=data['meta'].pagination.total_pages;
+          for (let index = 0; index < total; index++) {
+            this.total_pages.push({no:total+1}); 
+          }
+          console.log(this.datanya);
+        //   this.sdlclist();
+      }, err => {
+          console.log(err);
+      })    
+    }
+    next(){
+        // console.log(this.link['next'])
+        this.datanya=[];
+        this.http.get<any>(this.link['links']['next']+'&id_user='+this.iduser).subscribe(data => {
+          let datanya2 =data['data'];
+            this.datanya=datanya2;
+            this.link=data['meta'].pagination;
+            // console.log(this.datanya);
+            this.sdlclist();
+        }, err => {
+            console.log(err);
+        })    
+      }
+      prev(){
+        // console.log(this.link['previous'])
+        this.datanya=[];
+        this.http.get<any>(this.link['links']['previous']+'&id_user='+this.iduser).subscribe(data => {
+          let datanya2 =data['data'];
+            this.datanya=datanya2;
+            this.link=data['meta'].pagination;
+            // console.log(this.datanya);
+            this.sdlclist();
+        }, err => {
+            console.log(err);
+        })   
+      }
+      hitung(i){
+        this.datanya=[];
+        this.http.get<any>(this.url.apiurl+'/project/tampil?id_user='+ this.iduser +'&page='+i).subscribe(data => {
+          let datanya2 =data['data'];
+            this.datanya=datanya2;
+            this.link=data['meta'].pagination;
+            console.log(this.datanya);
+            this.sdlclist();
+        }, err => {
+            console.log(err);
+        })   
+      }
 }
